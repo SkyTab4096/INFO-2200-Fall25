4e7877d0e35247efa0701bd74be53a942c78c416
Kory <korya4096@gmail.com>
2025-11-10 19:36:48 -0700

    M05 Assignment

--- M05/2200_AndersonK_Assignment05/JellyBellyDB.cs
+++ M05/2200_AndersonK_Assignment05/JellyBellyDB.cs
@@ -1,3 +1,13 @@
+/*
+Name: Kory Anderson
+Class: INFO 2200
+Section: 001
+Professor: Sharp
+Date: November 10, 2025
+Participation or Assignment #: 05
+By submitting this assignment, I declare that the source code contained in this assignment was written solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment instructions, nor obtained from a subscription service. I understand that copying any source code, in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy.
+*/
+
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
